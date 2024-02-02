ada302eb9a9d69272cb13671f5bdf5cda2179735
alanaviolette <alanaviolette@users.noreply.github.com>
2024-02-02 08:50:06 +0100

    test : ID_MDP

---
+++ src/projetsis/ID_MDP.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetsis;
+
+
+public class ID_MDP {
+    private String ID;
+    private String MDP;
+    
+    
+    public ID_MDP(){
+        ID = "";
+        MDP = "";
+        
+    }
+    
+   
+    public String getID() {
+        return ID;
+    }
+
+    /**
+     * @return the MDP
+     */
+    public String getMDP() {
+        return MDP;
+    }
+            
+            
+}
