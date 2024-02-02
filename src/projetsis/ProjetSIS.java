1570cb0f177ee6647e333e5cbdd7435aa2f0f964
alanaviolette <alanaviolette@users.noreply.github.com>
2024-02-02 08:25:08 +0100

    test : int number;

--- src/projetsis/ProjetSIS.java
+++ src/projetsis/ProjetSIS.java
@@ -16,6 +16,9 @@ public class ProjetSIS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        int number;
+        
     }
     
 }
