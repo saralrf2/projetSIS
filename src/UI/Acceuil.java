64007d41e53d622242b60d2a4d03e98e385eab76
alanaviolette <alanaviolette@users.noreply.github.com>
2024-02-13 16:18:50 +0100

    changement acceuil

--- src/UI/Acceuil.java
+++ src/UI/Acceuil.java
@@ -337,7 +337,7 @@ public class Acceuil extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Acceuil().setVisible(true);
+A                new Acceuil().setVisible(true);
             }
         });
     }
